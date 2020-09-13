34d55b8bd04e6358326749db67bb1e50dfc49ca9
Nemo-Brunschwig <71225519+Nemo-Brunschwig@users.noreply.github.com>
2020-09-13 21:50:24 +0200

    Delete setBienvenue.js

--- Bot Discord/commands/setBienvenue.js
+++
@@ -1,8 +0,0 @@
-if (msg.content.startsWith(prefix + "setBienvenue")) {
-  var mbvn = msg.content.split("setBienvenue ");
-  db.get('guild')
-    .push({ id: msg.guild.name, bvn: mbvn[1]})
-    .write();
-
-  msg.reply(`salon de bienvenue configuré pour ${mbvn[1]}`)
-}
