88507d363dbe88703d6e097701dc4e4c8bf4cbf5
Nemo-Brunschwig <71225519+Nemo-Brunschwig@users.noreply.github.com>
2020-09-13 21:50:33 +0200

    Delete test.js

--- Bot Discord/commands/test.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, msg, args) => {
-  msg.channel.send(`msg : ${msg}`);
-    msg.channel.send(`args : ${args.join(" ")}`);
-      msg.channel.send(`test : ${args.shift()}`);
-        msg.channel.send(`test2 : ${args}`);
-}
